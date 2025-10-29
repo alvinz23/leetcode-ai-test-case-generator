c42dd34ddfa4ba1773dbcd2d4bc993cede968c86
alvin <alvinzhu65@gmail.com>
2025-10-29 19:37:56 -0400

    fresh branch, v2

--- backend/problems/urls.py
+++
@@ -1,9 +0,0 @@
-# backend/problems/urls.py
-
-from django.urls import path
-from . import views  # Import your views if you have any
-
-urlpatterns = [
-    path('', views.get_problem_data, name='problems'),
-    path('generate-test-case/<slug:slug>/', views.generate_test_case, name='generate_test_case'),
-]
